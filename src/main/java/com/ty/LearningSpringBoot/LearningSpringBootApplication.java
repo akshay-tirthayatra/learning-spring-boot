556402dd8b8fb1f7090b883ce1c0db446dea6423
akshay-tirthayatra <akshaykamatha@tirthayatra.org>
2025-05-30 15:47:32 +0530

    Spring Boot - Create a REST Controller
    print 'Hello World' on Web Browser

--- src/main/java/com/ty/LearningSpringBoot/LearningSpringBootApplication.java
+++ src/main/java/com/ty/LearningSpringBoot/LearningSpringBootApplication.java
@@ -1,7 +1,10 @@
 package com.ty.LearningSpringBoot;
 
+import com.ty.LearningSpringBoot.rest.FirstRestController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class LearningSpringBootApplication {
@@ -11,3 +14,4 @@ public class LearningSpringBootApplication {
     }
 
 }
+
